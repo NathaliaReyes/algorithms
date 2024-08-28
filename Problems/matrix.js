d7a20136fba75e0c1b0ea61a84f484658f79c3ef
Silvia Reyes <104657695+NathaliaReyes@users.noreply.github.com>
2024-08-28 15:16:37 -0600

    adding a better solution with Math.min and spread operator

--- Problems/matrix.js
+++ Problems/matrix.js
@@ -13,37 +13,33 @@ let arr = [
 function matrix(arr){ 
     let maxValues = []; 
     let minValues = new Array(arr[0].length).fill(Infinity);
+    console.log(minValues);
     for (let i = 0; i < arr.length; i++) {
         let max = arr[i][0]; // Initialize as the first element of the row
-        // console.log(max);
+        console.log(max);
         for (let j = 1; j < arr[i].length; j++) {
-            if (arr[i][j] > max) {
-                max = arr[i][j];
+            if (arr[i][j] > max) { //2<1 || 2<4 ||5<3
+                max = arr[i][j];  //arr[i][j] is the 2nd position, it will push 2,4,5
             }
         }
         maxValues.push(max); 
     }
-    console.log(maxValues);
+    console.log('max values:',maxValues);
     // Search for the smallest number in the column
-    for (let j = 0; j < arr[0].length; j++) {
+    for (let j = 0; j < arr.length; j++) { //iterates [1,2] 
         for (let i = 0; i < arr.length; i++) {
             if (arr[i][j] < minValues[j]) {
+                console.log(arr[i][j]) //1  //2
+                // console.log(minValues[j])
                 minValues[j] = arr[i][j];
             }
         }
     }
-    console.log(minValues);
+    console.log('min Values: ',minValues);
 
     // Now find both: the largest number in teh row and samellest in the column
-    let result = null;
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            if (arr[i][j] === maxValues[i] && arr[i][j] === minValues[j]) {
-                result = arr[i][j];
-            }
-        }
-    }
-
+    // find from the maxValues the smallest one: [2, 4, 5]
+    let result = Math.min(...maxValues);
     console.log(result);
 }
 matrix(arr);
